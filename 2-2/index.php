783e9b3c0b7fca50c9b26f4f37c9d1f51dbdbfe1
minimameko <67815882+minimameko@users.noreply.github.com>
2020-07-31 23:44:14 +0900

    課題提出
    2－2　変数とデータ型　課題提出

---
+++ 2-2/index.php
@@ -0,0 +1,37 @@
+<?php
+//index.php
+$message = "Hello World!!";
+var_dump($message);
+echo "<br>";
+
+$x = 8;
+$y = 5.55;
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+
+$flag = true;
+var_dump($flag);
+echo "<br>";
+
+$emp = null;
+var_dump($emp);
+echo "<br>";
+
+$en_words = "Hello World";
+var_dump($en_words);
+echo "<br>";
+$ja_words = "こんにちは";
+var_dump($ja_words);
+echo "<br>"."<br>"
+?>
+
+<?php
+//2－2課題
+$words_1 = "1.API";
+echo $words_1."<br>";
+echo "Application Programming Interfaceの略。ソフトウェアからOSの機能を利用するための仕様またはインターフェースの総称で、アプリケーションの開発を容易にするためのソフトウェア資源のこと"."<br>";
+$words_2 = "2. SVN（Subversion）";
+echo $words_2."<br>";
+echo "オープンソースの集中型バージョン管理システム。複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントを管理するのに用いられる。";
