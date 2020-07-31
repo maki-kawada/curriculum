582f61cfe7257089eaf5b1f392583fbd134fa141
minimameko <67815882+minimameko@users.noreply.github.com>
2020-07-31 23:06:36 +0900

    課題提出
    Hello Worldの課題提出

---
+++ 2-1/index.php
@@ -0,0 +1,28 @@
+<?php
+//PHPの研修カリキュラム2－1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2－1
+*/
+?>
+
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo "開発環境・・・システム開発を行う環境。基本的にローカル環境に構築する。"."<br>"."検証環境・・・開発環境で作られたシステムのテストを行うための環境。"."<br>"."本番環境・・・ユーザーが実際にシステムを利用する環境"."<br>";
+echo "2.基本設計書・詳細設計書"."<br>";
+echo "基本設計書・・・顧客の要件を実現するためのシステム構成や機能を具体化しまとめたもの。クライアントに見せる前提で作られる、外部向けの資料。"."<br>"."詳細設計書・・・基本設計書をもとに、システムの詳細をまとめた内部向け資料。この資料をもとに、プログラマーがシステム開発を行う。"."<br>";
+echo "3.Ajax"."<br>";
+echo "あるWebページを表示した状態のまま、別のページや再読込などを伴わずにWebサーバ側と通信を行い、動的に表示内容を変更する手法。javascriptの拡張機能を用いる。"
+
+?><img class="alignnone size-full wp-image-371" 
+src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" 
+height="180" data-pagespeed-url-hash="608921261" 
+onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
