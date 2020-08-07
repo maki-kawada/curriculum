f5b63575eeb9fe45a6c4a4cfbece0973391c9c42
minimameko <67815882+minimameko@users.noreply.github.com>
2020-08-07 23:19:09 +0900

    課題提出
    2－4　演算子の課題

---
+++ 2-4/index.php
@@ -0,0 +1,61 @@
+<?php
+//代入演算子
+$x = 5;
+echo $x;
+echo '<br>';
+echo "hello "."world";
+echo '<br>';
+$hello = "Hello ";
+$world = "World!!";
+echo $hello.$world;
+echo '<br>';
+?>
+
+<?php
+//四足演算子
+echo 2+4;
+echo '<br>';
+echo 3-1;
+echo '<br>';
+echo 6*7;
+echo '<br>';
+echo 9/3;
+echo '<br>';
+echo 14%3;
+echo '<br>';
+?>
+
+<?php
+//単項演算子
+$y = 2;
+$y++;
+echo $y;
+echo '<br>';
+$y--;
+echo $y;
+echo '<br>';
+$y += 5;
+echo $y;
+echo '<br>'
+?>
+
+<?php
+$z = 12;
+$z -= 4;
+echo $z;
+echo '<br>';
+$z /= 4;
+echo $z;
+echo '<br>';
+echo '<br>';
+?>
+
+<?php
+//IT用語
+echo "1．エビデンス".'<br>';
+echo "ITの分野では、システム開発のテスト工程などで、システムが正しく動作している、あるいは不具合が生じたことを示す証拠となる書類やデータなど".'<br>';
+echo "2. バッチ".'<br>';
+echo "バッチ処理はあらかじめ登録した一連の処理を自動的に実行する処理方式を指す。複数のプログラムやファイル転送コマンドなどの実行順序を定義し、大量のデータを一括処理する。処理の単位を「バッチ」と呼ぶ。".'<br>';
+echo "3. MVCモデル".'<br>';
+echo "機能ごとにプログラムの中身（処理）を分けて記述することで、プログラムを整理しやすくする考え方。分け方としては、「モデル(Model)」「ビュー(View)」「コントローラ(Controller)」の3種類";
+?>
